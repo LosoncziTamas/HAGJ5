0c6a520bb5897894288654bc31b11697461ea327
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-01-07 19:49:41 +0100

    add CharacterCollision.cs

---
+++ Assets/Code/CharacterCollision.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Code
+{
+    public class CharacterCollision : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            
+        }
+    }
+}
